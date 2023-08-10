93c1a8372b389a4150cb3dbe26a6c5fb29a85ba3
Azrul Amaline <azrul.amaline16@gmail.com>
2023-08-10 14:29:40 +0600

    10Aug2023

--- mypro/settings.py
+++ mypro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lug%@g10e7_uft6*quo_k^c9-hk!w=%#p(d+jvc89f^ut2kfkd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
